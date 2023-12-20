ce69f5c2eb45afdc959893470acbb91193308e9f
Alexandre Henrique Lopes <lopesah@gmail.com>
2023-12-20 09:26:46 -0300

    Atualização 19-12-2023 v4

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -3,8 +3,11 @@ import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosLogin from "./registrarEventos/login.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
 
 import io from "./servidor.js";
+
+io.use(autorizarUsuario);
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
